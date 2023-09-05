a51035ce2a4e51e7cc24837ae83135744916d4f5
Jinri <127276737+zzznly@users.noreply.github.com>
2023-09-05 20:17:10 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,18 +1,15 @@
 function solution(n) {
-    let answer = [];
-    let p = n;
+    let answer = [n];
     
-    while (p !== 1) {
-        answer.push(p);
-
-        if (p % 2 === 0) {
-            p /= 2;
+    while (n !== 1) {
+        if (n % 2 === 0) {
+            n /= 2;
         } else {
-            p = 3 * p + 1;
+            n = 3 * n + 1;
         }
+        
+        answer.push(n);
     }
-    
-    answer.push(1)
         
     return answer;
 }
