ad4ef51bd74ee31b93c80957a01634643492ad28
zzznly <zzznly@gmail.com>
2023-09-06 11:04:52 +0900

    [주사위게임 2] 다른사람 풀이 추가

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/others.js
@@ -0,0 +1,15 @@
+// 다른 사람의 풀이
+
+function solution(a, b, c) {
+  if (a === b && b === c) {
+    return (
+      (a + b + c) *
+      (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) *
+      (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    );
+  } else if (a === b || b === c || c === a) {
+    return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+  } else {
+    return a + b + c;
+  }
+}
