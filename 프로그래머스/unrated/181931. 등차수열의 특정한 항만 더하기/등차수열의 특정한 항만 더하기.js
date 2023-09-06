7125b1f984d68fed28c4f1c74e570f460e9562a8
zzznly <zzznly@gmail.com>
2023-09-06 19:56:32 +0900

    [등차수열 특정 항 더하기] 다른사람 풀이 추가

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,10 +1,17 @@
 function solution(a, d, included) {
-    let answer = 0;
-    let arr = [];
-    
-    for(let i = 0; i < included.length; i++) {
-        arr.push(a + d * i)
-    }
-    
-    return arr.filter((_, i) => included[i] === true).reduce((p, c) => p + c);
+  let answer = 0;
+  let arr = [];
+
+  for (let i = 0; i < included.length; i++) {
+    arr.push(a + d * i);
+  }
+
+  return arr.filter((_, i) => included[i]).reduce((p, c) => p + c);
+}
+
+// 다른 사람의 풀이
+function solution(a, d, included) {
+  return included.reduce((acc, flag, i) => {
+    return flag ? acc + a + d * i : acc;
+  }, 0);
 }
