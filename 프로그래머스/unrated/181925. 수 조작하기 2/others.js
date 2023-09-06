41c052a3e5864cba0abcce02998c3e83d71ddbe0
zzznly <zzznly@gmail.com>
2023-09-06 13:25:37 +0900

    [수 조작하기 2] 다른사람 풀이 추가

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/others.js
@@ -0,0 +1,18 @@
+// 다른 사람의 풀이
+function solution(numLog) {
+  const convert = {
+    1: "w",
+    "-1": "s",
+    10: "d",
+    "-10": "a",
+  };
+
+  return numLog
+    .slice(1)
+    .map((v, i) => {
+      return convert[v - numLog[i]];
+    })
+    .join("");
+}
+
+console.log(solution([0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1])); // wsdawsdassw
--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,18 +1,17 @@
-const calc = {
-    '1': 'w',
-    '-1': 's',
-    '10': 'd',
-    '-10': 'a',
-}
-
 function solution(numLog) {
-    let answer = '';
-    
-    for(let i = 0; i < numLog.length - 1; i++) {
-        let sub = numLog[i+1] - numLog[i];
-        
-        answer += calc[sub];
-    }
-    
-    return answer
+  let answer = "";
+  const calc = {
+    1: "w",
+    "-1": "s",
+    10: "d",
+    "-10": "a",
+  };
+
+  for (let i = 0; i < numLog.length - 1; i++) {
+    let sub = numLog[i + 1] - numLog[i];
+
+    answer += calc[sub];
+  }
+
+  return answer;
 }
