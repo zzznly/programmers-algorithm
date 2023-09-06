1b48207098c09175005b8a07a7f1b6a65b6e6221
zzznly <zzznly@gmail.com>
2023-09-06 14:34:05 +0900

    [날짜 비교하기] 다른사람의 풀이 추가

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,23 +1,30 @@
 function solution(date1, date2) {
-    let answer = 0;
-    
-    let advance;
-    
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    
-    if (year1 < year2) {
-        answer = 1;
-    } else if (year1 > year2) {
-        answer = 0;
-    } else {
-        if (month1 < month2) answer = 1;
-        else if (month1 > month2) answer = 0;
-        else {
-            if(day1 < day2) answer = 1;
-            else answer = 0;
-        }
+  let answer = 0;
+
+  const [year1, month1, day1] = date1;
+  const [year2, month2, day2] = date2;
+
+  if (year1 < year2) {
+    answer = 1;
+  } else if (year1 > year2) {
+    answer = 0;
+  } else {
+    if (month1 < month2) answer = 1;
+    else if (month1 > month2) answer = 0;
+    else {
+      if (day1 < day2) answer = 1;
+      else answer = 0;
     }
-    
-    return answer;
+  }
+
+  return answer;
 }
+
+// 다른사람의 풀이
+const solution = (date1, date2) => {
+  return new Date(date1) < new Date(date2) ? 1 : 0;
+};
+
+const solution = (date1, date2) => {
+  return +(new Date(date1) < new Date(date2));
+};
