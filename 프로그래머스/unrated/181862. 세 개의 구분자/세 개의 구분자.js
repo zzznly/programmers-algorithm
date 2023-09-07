4aa6b3a17574bd1f106746bf6af44bf39e90cff7
zzznly <zzznly@gmail.com>
2023-09-07 14:21:35 +0900

    [세개의 구분자] 다른사람 풀이 추가

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,3 +1,10 @@
 function solution(myStr) {
-    return myStr.split(/[a|b|c]/g).filter(v=>v).length ? myStr.split(/[a|b|c]/g).filter(v=>v) : ["EMPTY"]
+  return myStr.split(/[a|b|c]/g).filter(v => v).length
+    ? myStr.split(/[a|b|c]/g).filter(v => v)
+    : ["EMPTY"];
+}
+
+// 다른 사람의 풀이
+function solution(str) {
+  return str.match(/[^a-c]/g) || ["EMPTY"];
 }
