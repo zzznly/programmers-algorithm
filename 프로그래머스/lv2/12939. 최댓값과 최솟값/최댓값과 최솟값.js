532218797a98c6973154beea0a38d5823e174833
zzznly <zzznly@gmail.com>
2023-09-07 15:45:03 +0900

    [lv2][최대최소값] 다른사람풀이 추가

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,12 @@
-const solution = (s) => {
-    
-    const split = [...s.split(' ')].map(v => +v);
-    
-    return Math.min(...split).toString() + ' ' + Math.max(...split).toString()
-}
+const solution = s => {
+  const split = [...s.split(" ")].map(v => +v);
+
+  return Math.min(...split).toString() + " " + Math.max(...split).toString();
+};
+
+// 다른 사람의 풀이
+const solution2 = s => {
+  const arr = s.split(" ");
+
+  return Math.min(...arr) + " " + Math.max(...arr); // Math.max, min은 문자열을 취급한다!
+};
