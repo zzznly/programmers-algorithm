2ab5cae2eaaea81e145bfdd91b5bf16dbdaa77a2
zzznly <zzznly@gmail.com>
2023-09-07 14:27:19 +0900

    [문자열 반복 출력하기] 다른사람 풀이 추가

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -1,17 +1,21 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    str = input[0];
-    n = Number(input[1]);
-    
-    const res = Array(n).fill(str).join('')
-    console.log(res)
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  str = input[0];
+  n = Number(input[1]);
+
+  const res = Array(n).fill(str).join("");
+
+  // 다른 사람의 풀이
+  // const res = str.repeat(n); // String.prototoype.repeat(count) 내장함수!
+
+  console.log(res);
 });
