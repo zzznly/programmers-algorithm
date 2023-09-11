8d607933e8461a569ec2eba11611ca7d721b4764
Jinri <127276737+zzznly@users.noreply.github.com>
2023-09-11 09:58:35 +0900

    [unrated] Title: 배열 만들기 4, Time: 2.78 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    let stk = [];
+    
+    let i = 0;
+    while (arr.length > i) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+        } else {
+            if (stk[stk.length - 1] < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            } else {
+                stk.pop()
+            }
+        }
+    }
+    
+    return stk
+}
