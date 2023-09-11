dd7f8e28217fc086976e84ef64a6a789c1ffb0db
zzznly <zzznly@gmail.com>
2023-09-11 10:07:58 +0900

    [배열만들기4] 다른사람 풀이 추가

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,20 +1,63 @@
 function solution(arr) {
-    let stk = [];
-    
-    let i = 0;
-    while (arr.length > i) {
-        if (stk.length === 0) {
-            stk.push(arr[i]);
-            i++;
-        } else {
-            if (stk[stk.length - 1] < arr[i]) {
-                stk.push(arr[i]);
-                i++;
-            } else {
-                stk.pop()
-            }
-        }
+  let stk = [];
+
+  let i = 0;
+  while (i < arr.length) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      i++;
+    } else {
+      if (stk[stk.length - 1] < arr[i]) {
+        stk.push(arr[i]);
+        i++;
+      } else {
+        stk.pop();
+      }
     }
-    
-    return stk
+  }
+
+  return stk;
+}
+
+// 다른 사람의 풀이
+function solution2(arr) {
+  let stk = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      continue;
+    }
+
+    if (stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      continue;
+    }
+
+    stk.pop();
+    i--;
+  }
+
+  return stk;
+}
+
+function solution3(arr) {
+  let stk = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      continue;
+    } else {
+      if (stk[stk.length - 1] < arr[i]) {
+        stk.push(arr[i]);
+        continue;
+      } else {
+        stk.pop();
+        i--;
+      }
+    }
+  }
+
+  return stk;
 }
