703979e308f3a27b0efe9825e4c7b99c4b4d8c9b
Jinri <127276737+zzznly@users.noreply.github.com>
2023-09-11 14:17:13 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 22.15 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,10 @@
+function solution(arr, queries) {
+    let answer = [];
+    
+    for (let [s,e,k] of queries) {
+        let temp = arr.filter((v, i) => (i >= s && i <= e && v > k)).sort((a,b) => a-b)[0]
+        answer.push(temp ? temp : -1)
+    }
+    
+    return answer
+}
