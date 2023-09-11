396b0bba5280d9af9a6e6e017da609c77289cf9d
zzznly <zzznly@gmail.com>
2023-09-11 15:04:38 +0900

    [무작위 K개수 뽑기] 다른사람 풀이 추가

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,11 +1,24 @@
 function solution(arr, k) {
-    let answer = [];
-    
-    const set = [...new Set(arr)];
-    
-    for (let i = 0; i < k; i++) {
-        answer.push(set[i] ?? -1)
-    }
-    
-    return answer;
+  let answer = [];
+
+  const set = [...new Set(arr)];
+
+  for (let i = 0; i < k; i++) {
+    answer.push(set[i] ?? -1);
+  }
+
+  return answer;
+}
+
+// 다른 사람의 풀이
+function solution2(arr, k) {
+  const set = new Set(arr);
+
+  return set.size < k
+    ? [...Array.from(set), ...Array(k - set.size).fill(-1)]
+    : [...set.slice(0, k)];
+}
+
+function solution3(arr, k) {
+  return [...Array.from(new Set(arr)), ...Array(k).fill(-1)].slice(0, k);
 }
