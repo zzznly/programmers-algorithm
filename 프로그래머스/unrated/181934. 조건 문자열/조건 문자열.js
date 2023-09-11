d780f3099248cb70b482fcbeb29e3649aa097223
zzznly <zzznly@gmail.com>
2023-09-11 11:53:47 +0900

    [조건문자열] 다른사람의 풀이 추가

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -1,8 +1,20 @@
 function solution(ineq, eq, n, m) {
-    const ineqObj = {
-        "<": (a,b) => eq === '=' ? a <= b : a < b,
-        ">": (a,b) => eq === '=' ? a >= b : a > b,
-    }
-    
-    return ineqObj[ineq](n,m) ? 1 : 0
+  const ineqObj = {
+    "<": (a, b) => (eq === "=" ? a <= b : a < b),
+    ">": (a, b) => (eq === "=" ? a >= b : a > b),
+  };
+
+  return ineqObj[ineq](n, m) ? 1 : 0;
+}
+
+// 다른 사람의 풀이
+const operation = {
+  ">=": (x, y) => x >= y,
+  "<=": (x, y) => x <= y,
+  ">!": (x, y) => x > y,
+  "<!": (x, y) => x < y,
+};
+
+function solution(ineq, eq, n, m) {
+  return operation[ineq + eq](n, m) ? 1 : 0;
 }
