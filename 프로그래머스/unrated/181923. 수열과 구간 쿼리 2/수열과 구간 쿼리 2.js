eeeac002b69fbe1cb40fcd0a21b7b1426154b39d
zzznly <zzznly@gmail.com>
2023-09-11 14:28:38 +0900

    [수열구간쿼리2] 다른사람 풀이 추가

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,10 +1,23 @@
 function solution(arr, queries) {
-    let answer = [];
-    
-    for (let [s,e,k] of queries) {
-        let temp = arr.filter((v, i) => (i >= s && i <= e && v > k)).sort((a,b) => a-b)[0]
-        answer.push(temp ? temp : -1)
-    }
-    
-    return answer
+  let answer = [];
+
+  for (let [s, e, k] of queries) {
+    let temp = arr
+      .filter((v, i) => i >= s && i <= e && v > k)
+      .sort((a, b) => a - b)[0];
+    answer.push(temp ? temp : -1);
+  }
+
+  return answer;
+}
+
+// 다른 사람의 풀이
+function solution(arr, queries) {
+  return queries.map(
+    ([s, e, k]) =>
+      arr
+        .slice(s, e + 1)
+        .filter(v => v > k)
+        .sort((a, b) => a - b)[0] || -1
+  );
 }
