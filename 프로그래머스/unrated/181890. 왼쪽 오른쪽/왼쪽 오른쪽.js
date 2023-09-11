49167353fb454b45a23cee4acadee19d933ef8f9
zzznly <zzznly@gmail.com>
2023-09-11 11:22:19 +0900

    [왼쪽오른쪽] 다른 사람의 풀이 추가

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,11 +1,20 @@
 function solution(str_list) {
-    let answer = [];
-    
-    const idx = str_list.findIndex(v => v === 'l' || v === 'r');
-    
-    if (idx === -1) return [];
-    if (str_list[idx] === 'l') answer = str_list.slice(0, idx)
-    else answer = str_list.slice(idx + 1)
-    
-    return answer;
+  let answer = [];
+
+  const idx = str_list.findIndex(v => v === "l" || v === "r");
+
+  if (idx === -1) return [];
+  if (str_list[idx] === "l") answer = str_list.slice(0, idx);
+  else answer = str_list.slice(idx + 1);
+
+  return answer;
+}
+
+// 다른 사람의 풀이
+function solution(str_list) {
+  for (let i = 0; i < str_list.length; i++) {
+    if (str_list[i] === "l") return str_list.slice(0, i);
+    if (str_list[i] === "r") return str_list.slice(i + 1);
+  }
+  return [];
 }
