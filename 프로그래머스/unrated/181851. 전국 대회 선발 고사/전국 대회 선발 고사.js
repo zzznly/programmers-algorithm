76282d2e95d9e1a1d9dc21f2095b10c38c7fa405
Jinri <127276737+zzznly@users.noreply.github.com>
2023-09-15 00:15:29 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,5 +1,8 @@
 function solution(rank, attendance) {
-    const [a,b,c,...rest] = rank.filter((v, i) => attendance[i] === true).sort((a,b) => a-b).map(v => rank.indexOf(v))
-    
-    return 10000 * a + 100 * b + c
+  const [a, b, c] = rank
+    .filter((_, i) => attendance[i] === true)
+    .sort((a, b) => a - b)
+    .map(v => rank.indexOf(v));
+
+  return 10000 * a + 100 * b + c;
 }
