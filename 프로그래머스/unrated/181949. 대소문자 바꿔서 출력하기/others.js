83ca2f1ff2158ab53ccc1f68196b3664998b56e8
zzznly <zzznly@gmail.com>
2023-10-06 13:32:54 +0900

    [대소문자 바꿔서 출력하기] 다른 풀이 추가

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/others.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0].split("");
+
+  // 다른 사람의 풀이
+  str.forEach((value, index) => {
+    if (value === value.toUpperCase()) {
+      str[index] = value.toLowerCase();
+    } else {
+      str[index] = value.toUpperCase();
+    }
+  });
+
+  console.log(str.join(""));
+});
