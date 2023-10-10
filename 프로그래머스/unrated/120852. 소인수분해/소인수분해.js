92878bb2e64eb8e086e0fb640fda0d81c7529568
Jinri <127276737+zzznly@users.noreply.github.com>
2023-10-10 10:45:05 +0900

    [level unrated] Title: 소인수분해, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.js
@@ -1,12 +1,13 @@
-function solution(n) {    
+function solution(n) {
     let answer = [];
     
-    for(let i =2 ; i<=n ; i++) {
-        while( n % i === 0) {
+    for (let i = 2; i <= n; i++) {
+        while (n % i === 0) {
             answer.push(i);
-            n = n / i;
+            
+            n /= i
         }
     }
     
-    return [...new Set(answer)];
+    return [... new Set(answer)];
 }
