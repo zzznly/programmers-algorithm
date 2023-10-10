9fba3c860a685be26027a47cfd7cddff3d1b3c1f
Jinri <127276737+zzznly@users.noreply.github.com>
2023-10-10 11:07:36 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,9 @@
+function solution(my_str, n) {
+    let answer = [];
+    
+    for (let i = 0; i < my_str.length; i+=n) {
+        answer.push(my_str.substring(i, i+n))
+    }
+    
+    return answer;
+}
