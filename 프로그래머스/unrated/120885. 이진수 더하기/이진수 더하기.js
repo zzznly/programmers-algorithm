ba185fc377ec10cfbf1e80666996cafed3efc053
zzznly <zzznly@gmail.com>
2023-10-10 10:11:21 +0900

    [이진수더하기] parseInt, toString 설명 추가

--- 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,11 @@
 function solution(bin1, bin2) {
-    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+  return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
 }
+
+// parseInt()
+// 1. 정수로 변환
+// * 2. n 진수의 수를 10진수 정수로 변환 - ex. parseInt("1011", 2)
+
+// toString()
+// 1. 문자열로 변환
+// * 2. 숫자의 경우 선택적으로 기수(2~36)를 매개변수로 취하며, 이를 통해 10진수를 특정 진수로 변환한 값 반환 - ex. num.toString(2)
