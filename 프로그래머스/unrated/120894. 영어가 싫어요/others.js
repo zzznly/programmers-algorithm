3ee7c305cf1b6dd41dbc30c46747c2bc1aab217e
zzznly <zzznly@gmail.com>
2023-10-11 09:26:05 +0900

    [영어가 싫어요] 다른사람 풀이

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/others.js
@@ -0,0 +1,45 @@
+function solution(numbers) {
+  const num = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbers = numbers.split(num[i]).join(i);
+  }
+
+  return +numbers;
+}
+
+function solution1(numbers) {
+  const obj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  numbers = numbers.replace(
+    /zero|one|two|three|four|five|six|seven|eight|nine/g,
+    v => obj[v]
+  );
+
+  return numbers;
+}
+
+console.log(solution("onetwothreefourfivesixseveneightnine")); // 123456789
+console.log(solution("onefourzerosixseven")); // 14067
